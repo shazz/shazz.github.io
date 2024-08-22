9adfd16395fe8e08e2cb49e8bc1873a15de26662
shazz <604708+shazz@users.noreply.github.com>
2024-08-22 19:31:36 -0400

    Add files via upload

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,6 @@
 AUTHOR = "shazz"
 SITENAME = "Retrovirology"
 SITEURL = ""
-THEME = "themes/Peli-Kiera"
 THEME = "themes/pelican-chunk"
 
 PLUGIN_PATHS = ["pelican-plugins"]
@@ -11,6 +10,7 @@ PATH = "content"
 ARTICLE_PATHS = ["blog"]
 ARTICLE_SAVE_AS = "{date:%Y}/{slug}.html"
 ARTICLE_URL = "{date:%Y}/{slug}.html"
+STATIC_PATHS = ["pages", "images", "html"]
 
 TIMEZONE = "America/New_York"
 
@@ -37,6 +37,14 @@ SOCIAL = (
 )
 
 DEFAULT_PAGINATION = 10
+SUMMARY_MAX_LENGTH = 50
+SUMMARY_END_SUFFIX = "…"
+SUMMARY_MAX_PARAGRAPHS = None
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+DISPLAY_PAGES_ON_MENU = True
+PAGE_PATHS = ["pages"]
+
+EXTRA_PATH_METADATA = {"html/museum.html": {"path": "museum.html"}}
